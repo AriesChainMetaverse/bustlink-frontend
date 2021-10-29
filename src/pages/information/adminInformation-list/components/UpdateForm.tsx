975a8681164d266059deffc9a60ffd817d2977e2
godcong <jumbycc@163.com>
2021-10-29 18:39:41 +0800

    merge 2021/10/29
    merge last change

--- src/pages/information/adminInformation-list/components/UpdateForm.tsx
+++ src/pages/information/adminInformation-list/components/UpdateForm.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Form, Button, DatePicker, Input, Modal, Radio, Select, Steps ,Checkbox,Row,Col} from 'antd';
+import { Form, Button, DatePicker, Input, Modal, Radio, Select, Steps ,Checkbox,Row,Col,Space} from 'antd';
 
 import { TableListItem } from '../data.d';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 export interface FormValueType extends Partial<TableListItem> {
   target?: string;
@@ -38,11 +39,7 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
 
     video_no: props.values.video_no,
     id: props.values.id,
-    intro: props.values.intro,
-    lower_banner: props.values.lower_banner,
-    title: props.values.title,
-    top_right:props.values.top_right,
-    category:props.values.category,
+    frames_particulars:props.values.frames_particulars,
   });
 
   const [currentStep, setCurrentStep] = useState<number>(0);
@@ -64,136 +61,11 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
     const fieldsValue = await form.validateFields();
 
     setFormVals({ ...formVals, ...fieldsValue });
+    handleUpdate({ ...formVals, ...fieldsValue });
 
-    if (currentStep < 1) {
-      forward();
-    } else {
-      handleUpdate({ ...formVals, ...fieldsValue });
-    }
   };
 
   const renderContent = () => {
-    if (currentStep === 1) {
-      return (
-        <>
-          <FormItem name="category" label="分类"
-                    rules={[{ required: true, message: '请设置视频资源属性'}]}>
-            <Checkbox.Group>
-                <Row>
-                  <Col span={8}>
-                    <Checkbox
-                      value="newest"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-                    >
-                      最新上架
-                    </Checkbox>
-                  </Col>
-                  <Col span={8}>
-                    <Checkbox
-                      value="hottest"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-
-                    >
-                      人气最高
-                    </Checkbox>
-                  </Col>
-                  <Col span={8}>
-                    <Checkbox
-                      value="star"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-                    >
-                      明星
-                    </Checkbox>
-                  </Col>
-                  <Col span={8}>
-                    <Checkbox
-                      value="producer"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-                    >
-                      制作公司
-                    </Checkbox>
-                  </Col>
-                  <Col span={8}>
-                    <Checkbox
-                      value="exclusive"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-                    >
-                      独家内容
-                    </Checkbox>
-                  </Col>
-                  <Col span={8}>
-                    <Checkbox
-                      value="normal"
-                      style={{
-                        lineHeight: '32px',
-                      }}
-                    >
-                      正常
-                    </Checkbox>
-                  </Col>
-                </Row>
-            </Checkbox.Group>
-          </FormItem>
-          <FormItem name="lower_banner" label="底标">
-            <Select style={{ width: '100%' }}>
-              <Option value="free">免费</Option>
-              <Option value="discount">折扣</Option>
-              <Option value="event">限免</Option>
-              <Option value="premium">精品</Option>
-              <Option value="collection">收藏</Option>
-              <Option value="liked">喜欢</Option>
-              <Option value="none">无</Option>
-            </Select>
-          </FormItem>
-          <FormItem name="top_right" label="右上角标">
-            <Select style={{ width: '100%' }}>
-              <Option value="free">免费</Option>
-              <Option value="discount">折扣</Option>
-              <Option value="event">限免</Option>
-              <Option value="premium">精品</Option>
-              <Option value="collection">收藏</Option>
-              <Option value="liked">喜欢</Option>
-              <Option value="none">无</Option>
-            </Select>
-          </FormItem>
-
-        </>
-      );
-    }
-    // if (currentStep === 2) {
-    //   return (
-    //     <>
-    //       <FormItem
-    //         name="time"
-    //         label="开始时间"
-    //         rules={[{ required: true, message: '请选择开始时间！' }]}
-    //       >
-    //         <DatePicker
-    //           style={{ width: '100%' }}
-    //           showTime
-    //           format="YYYY-MM-DD HH:mm:ss"
-    //           placeholder="选择开始时间"
-    //         />
-    //       </FormItem>
-    //       <FormItem name="frequency" label="调度周期">
-    //         <Select style={{ width: '100%' }}>
-    //           <Option value="month">月</Option>
-    //           <Option value="week">周</Option>
-    //         </Select>
-    //       </FormItem>
-    //     </>
-    //   );
-    // }
     return (
       <>
         <FormItem
@@ -208,37 +80,95 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
         >
           <Input placeholder="请输入" disabled={true}/>
         </FormItem>
-        <FormItem
-          name="title"
-          label="标题"
-        >
-          <Input placeholder="请输入" />
-        </FormItem>
-        <FormItem
-          name="intro"
-          label="视频简介"
-          rules={[{ required: true, message: '请输入至少五个字符的描述！', min: 5 }]}
-        >
-          <TextArea rows={4} placeholder="请输入至少五个字符" />
-        </FormItem>
+        <Form.Item label='跳帧图片设置'>
+
+          <Form.List name="frames_particulars" >
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map(({ key, name, fieldKey, ...restField }) => (
+                  <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                    <Form.Item
+                      label='Index'
+                      {...restField}
+                      name={[name, 'index']}
+                      fieldKey={[fieldKey, 'index']}
+                      rules={[{ required: true, message: 'Missing index' }]}
+                    >
+                      <Input placeholder="index" type="number"/>
+                    </Form.Item>
+
+                    <Form.Item
+                      label='时间（单位s）'
+                      {...restField}
+                      name={[name, 'time']}
+                      fieldKey={[fieldKey, 'time']}
+                      rules={[{ required: true, message: 'Missing time' }]}
+                    >
+                      <Input placeholder="time" type="number" />
+                    </Form.Item>
+                    <Form.Item
+                      label='行号'
+
+                      {...restField}
+                      name={[name, 'row']}
+                      fieldKey={[fieldKey, 'row']}
+                      rules={[{ required: true, message: 'Missing row' }]}
+                    >
+                      <Input placeholder="row-NO" type="number"/>
+                    </Form.Item>
+                    <Form.Item
+                      label='列号'
+
+                      {...restField}
+                      name={[name, 'column']}
+                      fieldKey={[fieldKey, 'column']}
+                      rules={[{ required: true, message: 'Missing column' }]}
+                    >
+                      <Input placeholder="column-NO" type="number" />
+                    </Form.Item>
+                    <Form.Item
+                      label='标题'
+
+                      {...restField}
+                      name={[name, 'caption']}
+                      fieldKey={[fieldKey, 'caption']}
+                      rules={[{ required: false, message: 'Missing caption' }]}
+                    >
+                      <Input placeholder="caption" />
+                    </Form.Item>
+                    <MinusCircleOutlined onClick={() => remove(name)} />
+                  </Space>
+                ))}
+                <Form.Item>
+                  <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                    添加
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+        </Form.Item>
+
+
+
       </>
     );
   };
 
   const renderFooter = () => {
-    if (currentStep === 1) {
+    // if (currentStep === 1) {
       return (
         <>
-          <Button style={{ float: 'left' }} onClick={backward}>
-            上一步
-          </Button>
+          {/*<Button style={{ float: 'left' }} onClick={backward}>*/}
+          {/*  上一步*/}
+          {/*</Button>*/}
           <Button onClick={() => handleUpdateModalVisible(false, values)}>取消</Button>
           <Button type="primary" onClick={() => handleNext()}>
             完成
           </Button>
         </>
       );
-    }
+    // }
     // if (currentStep === 2) {
     //   return (
     //     <>
@@ -252,14 +182,14 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
     //     </>
     //   );
     // }
-    return (
-      <>
-        <Button onClick={() => handleUpdateModalVisible(false, values)}>取消</Button>
-        <Button type="primary" onClick={() => handleNext()}>
-          下一步
-        </Button>
-      </>
-    );
+    // return (
+    //   <>
+    //     <Button onClick={() => handleUpdateModalVisible(false, values)}>取消</Button>
+    //     <Button type="primary" onClick={() => handleNext()}>
+    //       下一步
+    //     </Button>
+    //   </>
+    // );
   };
 
   return (
@@ -267,30 +197,25 @@ const UpdateForm: React.FC<UpdateFormProps> = (props) => {
       width={640}
       bodyStyle={{ padding: '32px 40px 48px' }}
       destroyOnClose
-      title="视频资源上下架配置"
+      title="视频资源配置跳帧位置"
       visible={updateModalVisible}
       footer={renderFooter()}
       onCancel={() => handleUpdateModalVisible()}
     >
-      <Steps style={{ marginBottom: 28 }} size="small" current={currentStep}>
-        <Step title="基本信息" />
-        <Step title="配置分类" />
-      </Steps>
+
       <Form
         {...formLayout}
         form={form}
         initialValues={{
           video_no: formVals.video_no,
           id: formVals.id,
-          intro: formVals.intro,
-          lower_banner: formVals.lower_banner,
-          title:formVals.title,
-          top_right:formVals.top_right,
-          category:formVals.category,
+          frames_particulars:formVals.frames_particulars,
 
         }}
       >
         {renderContent()}
+
+
       </Form>
     </Modal>
   );
