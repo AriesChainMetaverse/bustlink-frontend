68bb0c4a5388e3964174be0379ce14aabc575075
godcong <jumbycc@163.com>
2021-11-15 20:01:25 +0800

    merge 2021/11/15
    merge last change

---
+++ src/locales/en-US/tips.ts
@@ -0,0 +1,11 @@
+export default {
+  'tip.add.success': 'Add Successed',
+  'tip.add.failed': 'Add failed',
+  'tip.add.adding': 'Adding',
+  'tip.set.success': 'Set Successed',
+  'tip.set.failed': 'Set failed',
+  'tip.set.setting': 'Setting',
+  'tip.delete.success': 'Delete Successed',
+  'tip.delete.failed': 'Delete failed',
+  'tip.delete.adding': 'Deleting',
+};
